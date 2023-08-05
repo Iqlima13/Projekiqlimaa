30dc32bd45626211541e22d515a730e52c803fd8
Iqlima13 <140933247+Iqlima13@users.noreply.github.com>
2023-08-05 15:17:24 +0700

    Add files via upload

---
+++ login.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Halaman login</title>
+  <link rel="stylesheet" href="assets/style.css">
+</head>
+<body>
+  
+  <!-- page login -->
+  <div class="page-login">
+    <!-- box-->
+    <div class="box">
+    Selamat Datang 
+    </div>
+    <!--box header-->
+    <div class="box-header">
+      
+    </div>
+    <!--box body-->
+    <div class="box-body">
+    <!-- form login--> 
+    <form>
+      
+    <div class="form-group">
+      <label>Username</label>
+      <input type="text" name="user" placeholder="Username" class="input-control">
+    </div>
+    <div class="form-group">
+      <label>Password</label>
+      <input type="password" name="pass" placeholder="Password"class="input-control">
+      <br>
+      <input type="submit"name="submit" value="Login">
+</br>
+      </form>
+    </div>
+  </div>
+</body>
+</html>
